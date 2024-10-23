850c0d657d1e96474fcef2360d89c9f2c89128dd
rpselser <rselser@terpmail.umd.edu>
2024-10-23 17:44:51 -0400

    Update script.js

--- script.js
+++ script.js
@@ -12,9 +12,47 @@ let showingTerm = true;
 
 // Start with this function to simply display the card
 function displayCard() {
+    let cardDiv = document.getElementById('card-content')
+
+    if (showingTerm === true){
+        cardDiv.innerHTML = 
+    `<p> ${flashcards[currentIndex].term} </p>`
+    
+    }else{
+    cardDiv.innerHTML = 
+    ` <p> ${flashcards[currentIndex].definition} </p> `
+}
+}
+
+document.getElementById('prev-btn').addEventListener('click', () => {
+    currentIndex = (currentIndex > 0) ? currentIndex - 1 : flashcards.length - 1
+    displayCard()
+});
+
+document.getElementById('next-btn').addEventListener('click', () => {
+    currentIndex = (currentIndex < flashcards.length - 1) ? currentIndex + 1 : 0
+    displayCard()
+});
+document.getElementById('flashcard').addEventListener('click', () => {
+    showingTerm = !showingTerm
+    displayCard()
+});
+
+
+document.getElementById('add-card-btn').addEventListener('click', ()=>{
+    const myTerm = document.getElementById('new-term').value
+    const myDef = document.getElementById('new-definition').value
+    flashcards.push({ term: myTerm, definition: myDef})
+
+    document.getElementById('new-term').value = ''
+    document.getElementById('new-definition').value = ''
+
+
 
 }
 
+
+)
 // The rest of the code you will write is apart of event listeners
 
 // This line will display the card when the page is refreshed
